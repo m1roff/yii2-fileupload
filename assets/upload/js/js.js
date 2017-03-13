14b4c947275d51ce136ce3cb7b2d339e8eb62017
Джасур Мирхамидов <mirkhamidov.jasur@gmail.com>
2017-03-13 23:04:09 +0300

    fixes in js

--- assets/upload/js/js.js
+++ assets/upload/js/js.js
@@ -1,16 +1,23 @@
 (function () {
     'use strict';
 
-    var $inputObject = $('.cp-file-upload-form');
-
-    $inputObject.on('fileuploaded', function(event, data, previewId, index) {
-        console.log('dododod');
-        console.log(data.response);
+    $(document).on('fileuploaded', '.cp-file-upload-form', function(event, data, previewId, index) {
         if (data.response && data.response.uploadedFiles) {
             for (var i in data.response.uploadedFiles) {
                 addFileInfoToForm(event, data.response.uploadedFiles[i], previewId);
             }
         }
+    });
+
+    $(document).on('filepreupload', '.cp-file-upload-form', function(event, data, previewId, index) {
+
+        if (window[$(this).data('krajeeFileinput')]['maxFileCount'] == 1) {
+            removeFileInputs(event);
+        }
+    });
+
+    $(document).on('filecleared', '.cp-file-upload-form', function(event) {
+        removeFileInputs(event);
     });
 
 
@@ -61,6 +68,31 @@
             newObj.attr({"data-file-id":id, "data-file-preview_id":previewId});
             currResultInput.after(newObj);
         }
+    }
+
+    function removeFileInputs(event)
+    {
+        let getCurrentList = function (event) {
+            return $('.cp-file-uploaded-list', $(event.target).parents('.file-upload-form:first'));
+        };
+        let currResultInput = getCurrentList(event);
+
+        if (currResultInput.length > 1) {
+            currResultInput.each(function () {
+                if (getCurrentList(event).length == 1) {
+                    return true;
+                }
+                $(this).remove();
+            });
+        }
+
+        currResultInput = getCurrentList(event);
+        currResultInput.val('');
+        currResultInput.attr({
+            "data-file-id":'',
+            "data-file-preview_id":'',
+            value: ''
+        });
     }
 
 })();
